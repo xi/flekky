fb8cd5e3891a14e2418f2968866dec43c7405c4d
Tobias Bengfort <tobias.bengfort@gmx.net>
2014-05-11 10:47:19 +0200

    pep8

--- flaky.py
+++ flaky.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 """Static website generator inspired by jekyll based on flask.
 
-Based on the tutorial on https://nicolas.perriault.net/code/2012/dead-easy-yet-powerful-static-website-generator-with-flask/.
+Based on the tutorial on https://nicolas.perriault.net/code/2012/dead-easy-\
+yet-powerful-static-website-generator-with-flask/.
 
 """
 
@@ -20,19 +21,23 @@ app.config.from_object(__name__)
 pages = FlatPages(app)
 freezer = Freezer(app)
 
+
 @app.route('/')
 def index():
     return render_template('index.html', pages=pages)
+
 
 @app.route('/tag/<string:tag>/')
 def tag(tag):
     tagged = [p for p in pages if tag in p.meta.get('tags', [])]
     return render_template('tag.html', pages=tagged, tag=tag)
 
+
 @app.route('/<path:path>/')
 def page(path):
     page = pages.get_or_404(path)
     return render_template('page.html', page=page)
+
 
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == "build":
